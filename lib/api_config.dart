ddbf4fc92f30a0e8df8ddc0541b3aced5c9e06a9
adiweb12 <vsadith051@gmail.com>
2026-02-25 16:45:23 +0530

    Delete lib directory

--- lib/api_config.dart
+++
@@ -1,6 +0,0 @@
-class ApiConfig {
-  static const String baseUrl = "https://onechatbackendultraproxysecurity.onrender.com"; 
-  
-  static const String loginUrl = "$baseUrl/login";
-  static const String signupUrl = "$baseUrl/signup";
-}
