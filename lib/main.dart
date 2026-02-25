0e2c6de2f662d15dab20c6fc64a593314952437a
adiweb12 <vsadith051@gmail.com>
2026-02-25 16:46:08 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+import 'screens/login_screen.dart';
+import 'screens/home_screen.dart';
+import 'services/auth_service.dart';
+import 'services/chat_service.dart';
+import 'services/theme_service.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  await Hive.openBox('messages');
+  await Hive.openBox('contacts_cache');
+  await Hive.openBox('app_data');
+
+  final prefs = await SharedPreferences.getInstance();
+  final token = prefs.getString('auth_token');
+  final isLoggedIn = token != null && token.isNotEmpty;
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthService()),
+        ChangeNotifierProvider(create: (_) => ChatService()),
+        ChangeNotifierProvider(create: (_) => ThemeService()),
+      ],
+      child: MyApp(isLoggedIn: isLoggedIn),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  final bool isLoggedIn;
+  const MyApp({super.key, required this.isLoggedIn});
+
+  @override
+  Widget build(BuildContext context) {
+    final themeService = Provider.of<ThemeService>(context);
+    return MaterialApp(
+      title: 'OneChat',
+      debugShowCheckedModeBanner: false,
+      theme: themeService.lightTheme,
+      darkTheme: themeService.darkTheme,
+      themeMode: themeService.themeMode,
+      home: isLoggedIn ? const HomeScreen() : const LoginScreen(),
+    );
+  }
+}
