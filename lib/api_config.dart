2bca5fa1f9fb2f90cb44581b3312ee79b7d82b81
adiweb12 <vsadith051@gmail.com>
2026-02-25 07:43:01 +0530

    Update api_config.dart

--- lib/api_config.dart
+++ lib/api_config.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
-  static const String baseUrl = "https://YOUR-APP-NAME.onrender.com"; 
+  static const String baseUrl = "https://onechatbackendultraproxysecurity.onrender.com"; 
   
   static const String loginUrl = "$baseUrl/login";
   static const String signupUrl = "$baseUrl/signup";
