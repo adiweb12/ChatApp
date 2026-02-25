0d4111237c6ad6ae9fddce16079230ed1f0b2acc
adiweb12 <vsadith051@gmail.com>
2026-02-25 06:57:23 +0530

    Create api_config.dart

---
+++ lib/api_config.dart
@@ -0,0 +1,6 @@
+class ApiConfig {
+  static const String baseUrl = "https://YOUR-APP-NAME.onrender.com"; 
+  
+  static const String loginUrl = "$baseUrl/login";
+  static const String signupUrl = "$baseUrl/signup";
+}
