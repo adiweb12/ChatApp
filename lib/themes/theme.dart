25e9388bdc9a2d36e0eda54fdeb476edec45b672
adiweb12 <vsadith051@gmail.com>
2026-02-27 07:34:11 +0530

    Add files via upload

--- lib/themes/theme.dart
+++ lib/themes/theme.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class AppTheme {
+  static ThemeData mainTheme = ThemeData(
+    brightness: Brightness.light,
+    primaryColor: Colors.white,
+    scaffoldBackgroundColor: Colors.green,
+    textTheme: const TextTheme(
+      bodyLarge: TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+  );
+}
