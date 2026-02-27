1010a06d87c8e94cd3afd7fe115f4021c1ff5fc0
adiweb12 <vsadith051@gmail.com>
2026-02-27 07:35:17 +0530

    Add files via upload

--- lib/functions/functions.dart
+++ lib/functions/functions.dart
@@ -1,1 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:onechat/models/models.dart';
+import 'package:onechat/screen/editor_page.dart';
 
+Future<bool> editMail({
+  required String phonenumber,
+  required String newMail,
+  required List<UserDetails> allUsers,
+}) async {
+  try {
+    UserDetails userToEdit = allUsers.firstWhere(
+      (user) => user.phoneNumber == phonenumber,
+    );
+    userToEdit.email = newMail;
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+Future<bool> loginLogic({
+  required String email,
+  required String password,
+  required List<UserDetails> allUsers,
+}) async {
+  try {
+    allUsers.firstWhere(
+      (user) => user.email == email && user.password == password,
+    );
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+Future<bool> updatePassword({
+  required String email,
+  required String newPassword,
+  required List<UserDetails> allUsers,
+}) async {
+  try {
+    UserDetails userToEdit = allUsers.firstWhere(
+      (user) => user.email == email,
+    );
+    userToEdit.password = newPassword;
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+Future<void> dropDownLogic(String value, BuildContext context) async {
+  switch (value) {
+    case 'editMail':
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const EditMailPage()),
+      );
+      break;
+    case 'editPass':
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const EditPassPage()),
+      );
+      break;
+  }
+}
