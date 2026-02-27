3fdd5a5541c4a0035c97bf58e33cd5c25d839975
adiweb12 <vsadith051@gmail.com>
2026-02-27 07:32:11 +0530

    Add files via upload

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,1 +1,24 @@
+class UserDetails {
+  final String id;
+  final String userName;
+  String phoneNumber;
+  String email;
+  String password; // Added password field which was missing
+  String dob;
 
+  UserDetails({
+    required this.id,
+    required this.userName,
+    required this.phoneNumber,
+    required this.email,
+    required this.password,
+    required this.dob,
+  });
+}
+
+// Global list for demo purposes
+List<UserDetails> globalUserList = [
+  UserDetails(id: "1", userName: "Admin", phoneNumber: "123456", email: "a@b.com", password: "123", dob: "01/01/2000")
+];
+
+bool isLoggedIn = false; 
