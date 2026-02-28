8939ee4d1e138750d12af67e16d964990ffffd97
adiweb12 <vsadith051@gmail.com>
2026-02-28 18:35:40 +0530

    Update models.dart

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -18,7 +18,7 @@ class UserDetails {
 
 // Global list for demo purposes
 List<UserDetails> globalUserList = [
-  UserDetails(id: "1", userName: "Admin", phoneNumber: "123456", email: "a@b.com", password: "123", dob: "01/01/2000")
+  UserDetails(id: "1", userName: "Adithvs", phoneNumber: "8138872364", email: "adith@gmail.com", password: "1234567", dob: "01/01/2000")
 ];
 
 bool isLoggedIn = false; 
