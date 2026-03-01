65017c9c3d975a74bd50e1bfb1dd62f57fc20766
adiweb12 <vsadith051@gmail.com>
2026-03-01 18:00:32 +0530

    Update api_urls.dart

--- lib/constant/api_urls.dart
+++ lib/constant/api_urls.dart
@@ -5,3 +5,7 @@ const loginBaseUrl = "$baseUrl/onechat/login/vdhj67";
 const signupBaseUrl = "$baseUrl/onechat/signup/vgtueb567";
 
 const refreshBaseUrl = "$baseUrl/onechat/refresh";
+
+const updateEmailUrl = "$baseUrl/onechat/update-email";
+
+const updatePasswordUrl = "$baseUrl/onechat/update-password";
