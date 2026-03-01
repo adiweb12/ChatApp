e9801ad0e59ec552189dfce30039db5f5267a64a
adiweb12 <vsadith051@gmail.com>
2026-03-01 13:37:12 +0530

    Create api_urls.dart

---
+++ lib/constant/api_urls.dart
@@ -0,0 +1,7 @@
+const baseUrl = "https://onechatbackendultraproxysecurity.onrender.com";
+
+const loginBaseUrl = "$baseUrl/onechat/login/vdhj67";
+
+const signupBaseUrl = "$baseUrl/onechat/signup/vgtueb567";
+
+const refreshBaseUrl = "$baseUrl/onechat/refresh";
