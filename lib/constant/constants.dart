b463b40b2e8ab9b6e0ec5ad1d7c2ea7e76c8d30f
adiweb12 <vsadith051@gmail.com>
2026-03-01 13:57:52 +0530

    Update constants.dart

--- lib/constant/constants.dart
+++ lib/constant/constants.dart
@@ -1,6 +1,6 @@
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+//import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 const SECRET_LOGIN_KEY = 'UserLoginInfo';
 const User_Id = 'USER_ID';
-const storage = FlutterSecureStorage(aOptions: AndroidOptions(encryptedSharedPreferences: true),
-);
+//const storage = FlutterSecureStorage(aOptions: AndroidOptions(encryptedSharedPreferences: true),
+//);
